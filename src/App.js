b779547330ba6e26dee69f676c24963f27d5a76d
AnnaElez <anna.elez.y@tumo.org>
2020-11-14 16:34:11 +0300

    second homework

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import Animals from './classes.jsx'
+
+export function Surname(props) {
+  // console.log(props)
+  return(
+    <>
+    <p>
+      {props.propsObj.lastName}
+    </p>
+    <p>
+      {props.propsObj.age}
+    </p>
+    </>
+  )
+}
+
+export function Name(props) {
+  return(
+    <>
+      <img src = {props.logo}/>
+    </>
+  )
+ 
+}
 
 function App() {
+  
+  const Greeting = "Hello";
+
+  let obj = {
+    lastName: "Elez",
+    age: "18"
+  }
+  
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Greeting}
+      <Name/>
+      <Surname propsObj = {obj}/>
     </div>
+
+    <div className="App">
+    <Animals type = 'cat' name = "Karen" color = "white" />
+    <Animals type = 'monkey' name = "Tim" color = "brown" />
+    <Animals type = 'hamster' name = "Vilyam" color = "orange" />
+    </div>
+    </>
   );
 }
 
