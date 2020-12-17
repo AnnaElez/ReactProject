43df1ca134583121cc148598c53e4203e29ddd81
AnnaElez <anna.elez.y@tumo.org>
2020-12-17 23:48:20 +0300

    Selected delete, delete, id and disable button

---
+++ src/CardTask.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { Card, Button } from 'react-bootstrap';
+import './Card.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+
+class CardTask extends Component {
+
+    state = {
+        checked:false
+
+    }
+
+
+    handleCheck=()=>{
+        this.setState({
+
+            checked:!this.state.checked
+        })
+
+        const {onCheck,data}=this.props
+
+        onCheck(data._id)
+    }
+
+    render(props) {
+
+        const task = this.props.data
+        const {checked} = this.state
+
+        return (
+            <Card className = {checked ? 'task':''}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <input
+                        type='checkbox'
+                        onClick={this.handleCheck}>
+
+                    </input>
+
+                    <Card.Title>
+                        {task.text.slice(0, 10) + '...'}
+                    </Card.Title>
+
+                    <Card.Text>
+                        {task.text}
+                    </Card.Text>
+
+                    <Button
+                        variant="danger"
+                        onClick={() => this.props.onRemove(task._id)}
+                    >Delete</Button>
+                </Card.Body>
+            </Card>
+        )
+
+    }
+}
+
+export default CardTask
--- src/Demo.js
+++ src/Demo.js
@@ -3,13 +3,14 @@ import c from './Demo.module.css';
 import { InputGroup, Button, FormControl, Col, Row, Container, Card } from 'react-bootstrap';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import idGenerator from './idGenerator.js'
-
+import Task from './CardTask.js'
 
 class ToDo extends Component {
     state = {
         tasks: [],
         inputValue: '',
         arr: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+        selectedTasks: new Set(),
     }
 
     handleChange = (event) => {
@@ -18,18 +19,34 @@ class ToDo extends Component {
         })
     }
 
+    handleCheck = (taskId) => {
+        const selectedTasks = new Set(this.state.selectedTasks)
+
+        if (selectedTasks.has(taskId)) {
+            selectedTasks.delete(taskId)
+        }
+
+        else {
+            selectedTasks.add(taskId)
+        }
+
+        this.setState({
+            selectedTasks //ete key=value karelia mi angam ira anun@ grel
+        })
+    }
+
 
     handleAdd = () => {
 
         const { inputValue } = this.state;
 
-        if(!inputValue){
+        if (!inputValue) {
             return
         }
 
-        const newTask ={
-            text:inputValue,
-            _id:idGenerator()
+        const newTask = {
+            text: inputValue,
+            _id: idGenerator()
 
         }
 
@@ -42,20 +59,36 @@ class ToDo extends Component {
         })
     }
 
-    handleKeyDown = (event) =>{
+    handleKeyDown = (event) => {
 
-        if (event.key==="Enter"){
+        if (event.key === "Enter") {
             this.handleAdd();
         }
 
 
     }
 
-    handleDelete = (taskId) =>{
+    handleDelete = (taskId) => {
 
-       const newTasks = this.state.tasks.filter(task => task._id !== taskId);
+        const newTasks = this.state.tasks.filter(task => task._id !== taskId);
         this.setState({
-            tasks:newTasks,
+            tasks: newTasks,
+        })
+
+    }
+
+    removeSelected = (taskId) => {
+
+        let tasks = [...this.state.tasks]
+
+        this.state.selectedTasks.forEach((id) => {
+
+            tasks = tasks.filter((task) => task._id !== id)
+        })
+
+        this.setState({
+            tasks,
+            selectedTasks: new Set()
         })
 
     }
@@ -63,59 +96,59 @@ class ToDo extends Component {
 
 
 
-render(props) {
+    render(props) {
 
-    const { inputValue } = this.state;
-    const tasksArray = this.state.tasks.map((task, i) => {
+        const { inputValue, selectedTasks } = this.state;
+        const tasksArray = this.state.tasks.map((task, i) => {
+
+            return (
+                <Col key={i} xs={12} sm={6} md={4} lg={3} xl={2} className='mb-3'>
+                    <Task onCheck={this.handleCheck} onRemove={this.handleDelete} data={task} />
+                </Col>
+            )
+        });
 
         return (
-            <Col key={i} xs={12} sm={6} md={4} lg={3} xl={2} className='mb-3'>
-                <Card className={c.task}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
-                    <Card.Body>
-                        <Card.Title>{task.text.slice(0, 10) + '...'}</Card.Title>
-                        <Card.Text>{task.text}</Card.Text>
-                        <Button variant="danger" onClick = {()=>this.handleDelete(task._id)}>Delete</Button>
-                    </Card.Body>
-                </Card>
-            </Col>
-        )
-    });
+            <div className={c.col}>
+                <Container>
+                    <Row className="justify-content-center">
+                        <Col lg={3} mg={8} xl={2} sm={6}>
+                            <InputGroup className={c.place}>
+                                <FormControl type="text"
+                                    placeholder={this.props.placeholder}
+                                    value={this.state.inputValue}
+                                    onChange={(event) => this.handleChange(event)}
+                                    aria-describedby="basic-addon1"
+                                    onKeyDown={(event) => this.handleKeyDown(event)}
+                                />
 
-    return (
-        <div className={c.col}>
-            <Container>
-                <Row className="justify-content-center">
-                    <Col lg={3} mg={8} xl={2} sm={6}>
-                        <InputGroup className={c.place}>
-                            <FormControl type="text" 
-                            placeholder={this.props.placeholder} 
-                            value={this.state.inputValue} 
-                            onChange={this.handleChange} 
-                            aria-describedby="basic-addon1"
-                            onKeyDown = {(event)=>this.handleKeyDown(event)}
-                            />
+                                <InputGroup.Prepend>
+                                    <Button
+                                        variant="outline-secondary"
+                                        type="button" value="Add"
+                                        onClick={this.handleAdd}
 
-                            <InputGroup.Prepend>
-                                <Button
-                                    variant="outline-secondary"
-                                    type="button" value="Add"
-                                    onClick={this.handleAdd}
-                                   
                                     >Add</Button>
-                            </InputGroup.Prepend>
+                                </InputGroup.Prepend>
 
-                        </InputGroup>
-                    </Col>
+                            </InputGroup>
+                        </Col>
 
-                </Row>
+                    </Row>
 
-                <Row>{tasksArray}</Row>
-            </Container>
-        </div>
+                    <Row>{tasksArray}</Row>
 
-    )
-}
+                    <Button variant="outline-danger"
+                        onClick={this.removeSelected}
+                        disabled={selectedTasks.size === 0 ? true : false} >
+                        Remove selected
+
+                </Button >
+                </Container>
+            </div>
+
+        )
+    }
 
 
 }
